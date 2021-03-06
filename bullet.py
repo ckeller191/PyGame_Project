bc8926b22fad078a6c729596e3fca10067d1dce5
Chris Keller <ckeller191@gmail.com>
2021-03-06 13:56:51 -0600

    fixed typo on init function

--- bullet.py
+++ bullet.py
@@ -5,7 +5,7 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
 
-    def __init(self, ai_game):
+    def __init__(self, ai_game):
         """Create a bullet object at the ship's current position."""
         super().__init__()
         self.screen = ai_game.screen
