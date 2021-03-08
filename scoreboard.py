4019d26782c8649a8577b5d0f194bce17a208591
Chris Keller <ckeller191@gmail.com>
2021-03-08 13:14:01 -0600

    add level to show score function

--- scoreboard.py
+++ scoreboard.py
@@ -37,6 +37,7 @@ class Scoreboard:
         """Draw the score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
