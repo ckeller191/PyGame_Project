e0b294a69a5fd4ec4384799611d3f516228e9453
Chris Keller <ckeller191@gmail.com>
2021-03-08 12:34:59 -0600

    define increase speed

--- settings.py
+++ settings.py
@@ -35,3 +35,8 @@ class Settings:
 
         # fleet direction of 1 represents right, -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
