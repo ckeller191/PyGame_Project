87b26ce9baeb9b833821d6bb663baec12d2ef793
Chris Keller <ckeller191@gmail.com>
2021-03-08 12:32:03 -0600

    init for static settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion"""
 
     def __init__(self):
-        # Initialize the game's settings
+        # Initialize the game's static settings.
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -21,6 +21,8 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left
-        self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dymanic_settings()
